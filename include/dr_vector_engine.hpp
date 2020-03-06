2b76faaef52f664e8b334694e8edc83afeb1fbe0
Thomas Cassimon <thomas.cassimon@outlook.com>
2020-03-06 19:57:08 +0100

    Further work on testing. Formatted code using clang-format.

--- include/dr_vector_engine.hpp
+++ include/dr_vector_engine.hpp
@@ -13,78 +13,78 @@ namespace std::math
 	class dr_vector_engine
 	{
 		public:
-			//-	Types
-			//
-			using engine_category		= resizable_vector_engine_tag;
-			using element_type			= T;
-			using value_type			= remove_cv_t<T>;
-			using allocator_type		= AT;
-			using pointer				= typename allocator_traits<AT>::pointer;
-			using const_pointer			= typename allocator_traits<AT>::const_pointer;
-			using reference				= element_type&;
-			using const_reference		= const element_type&;
-			using difference_type		= ptrdiff_t;
-			using size_type				= size_t;
-			// todo: These 2 are implementation defined, check if there's a better type for this
-			using iterator				= pointer;
-			using const_iterator		= const_pointer;
+		//-	Types
+		//
+		using engine_category = resizable_vector_engine_tag;
+		using element_type = T;
+		using value_type = remove_cv_t<T>;
+		using allocator_type = AT;
+		using pointer = typename allocator_traits<AT>::pointer;
+		using const_pointer = typename allocator_traits<AT>::const_pointer;
+		using reference = element_type&;
+		using const_reference = const element_type&;
+		using difference_type = ptrdiff_t;
+		using size_type = size_t;
+		// todo: These 2 are implementation defined, check if there's a better type for this
+		using iterator = pointer;
+		using const_iterator = const_pointer;
 
-			//-	Construct/copy/destroy
-			//
-			~dr_vector_engine();
+		//-	Construct/copy/destroy
+		//
+		~dr_vector_engine();
 
-			dr_vector_engine();
-			dr_vector_engine(dr_vector_engine&&) noexcept;
-			dr_vector_engine(const dr_vector_engine&);
+		dr_vector_engine();
+		dr_vector_engine(dr_vector_engine&&) noexcept;
+		dr_vector_engine(const dr_vector_engine&);
 
-			template <class U>
-			dr_vector_engine(initializer_list<U> list);
-			dr_vector_engine(size_type elems);
-			dr_vector_engine(size_type elems, size_type elem_cap);
+		template <class U>
+		dr_vector_engine(initializer_list<U> list);
+		dr_vector_engine(size_type elems);
+		dr_vector_engine(size_type elems, size_type elem_cap);
 
-			dr_vector_engine&	operator=	(dr_vector_engine&& rhs) noexcept;
-			dr_vector_engine&	operator=	(const dr_vector_engine& rhs);
+		dr_vector_engine& operator=(dr_vector_engine&& rhs) noexcept;
+		dr_vector_engine& operator=(const dr_vector_engine& rhs);
 
-			template <class ET2>
-			dr_vector_engine&	operator=	(const ET2&	rhs);
+		template <class ET2>
+		dr_vector_engine& operator=(const ET2& rhs);
 
-			//-	Iterators
-			//
-			iterator 		begin()				noexcept;
-			iterator		end()				noexcept;
-			const_iterator	begin() 	const	noexcept;
-			const_iterator	end()		const	noexcept;
-			const_iterator 	cbegin()	const	noexcept;
-			const_iterator	cend()		const	noexcept;
+		//-	Iterators
+		//
+		iterator begin() noexcept;
+		iterator end() noexcept;
+		const_iterator begin() const noexcept;
+		const_iterator end() const noexcept;
+		const_iterator cbegin() const noexcept;
+		const_iterator cend() const noexcept;
 
-			//-	Capacity
-			//
-			[[nodiscard]]	size_type	capacity()	const noexcept;
-			[[nodiscard]]	size_type	elements()	const noexcept;
+		//-	Capacity
+		//
+		[[nodiscard]] size_type capacity() const noexcept;
+		[[nodiscard]] size_type elements() const noexcept;
 
-			void reserve(size_type cap);
-			void resize(size_type elems);
-			void resize(size_type elems, size_type cap);
+		void reserve(size_type cap);
+		void resize(size_type elems);
+		void resize(size_type elems, size_type cap);
 
-			//-	Element access
-			reference		operator()	(size_type i);
-			const_reference	operator()	(size_type i) const;
+		//-	Element access
+		reference operator()(size_type i);
+		const_reference operator()(size_type i) const;
 
-			//- Modifiers
-			//
-			void swap(dr_vector_engine& rhs)				noexcept;
-			void swap_elements(size_type i, size_type j)	noexcept;
+		//- Modifiers
+		//
+		void swap(dr_vector_engine& rhs) noexcept;
+		void swap_elements(size_type i, size_type j) noexcept;
 
 		private:
-			constexpr static size_type DEFAULT_SIZE_ = 3;
+		constexpr static size_type DEFAULT_SIZE_ = 3;
 
-			allocator_type	alloc_;
-			pointer			data_;
-			size_type		elems_;
-			size_type		cap_;
+		allocator_type alloc_;
+		pointer data_;
+		size_type elems_;
+		size_type cap_;
 	};
 
-	template<class T, class AT>
+	template <class T, class AT>
 	dr_vector_engine<T, AT>::~dr_vector_engine()
 	{
 		if ((0 < this->cap_) && (this->data_ != nullptr))
@@ -93,17 +93,15 @@ namespace std::math
 		}
 	}
 
-	template<class T, class AT>
-	dr_vector_engine<T, AT>::dr_vector_engine():
-		dr_vector_engine(0, DEFAULT_SIZE_)
-	{}
+	template <class T, class AT>
+	dr_vector_engine<T, AT>::dr_vector_engine() : dr_vector_engine(0, DEFAULT_SIZE_)
+	{
+	}
 
-	template<class T, class AT>
-	dr_vector_engine<T, AT>::dr_vector_engine(dr_vector_engine&& other) noexcept:
-		alloc_(std::move(other.alloc_)),
-		data_(std::move(other.data_)),
-		elems_(std::move(other.elems_)),
-		cap_(std::move(other.cap_))
+	template <class T, class AT>
+	dr_vector_engine<T, AT>::dr_vector_engine(dr_vector_engine&& other) noexcept
+		: alloc_(std::move(other.alloc_)), data_(std::move(other.data_)), elems_(std::move(other.elems_)),
+		  cap_(std::move(other.cap_))
 	{
 		other.alloc_ = AT();
 		other.data_ = nullptr;
@@ -111,12 +109,10 @@ namespace std::math
 		other.cap_ = 0;
 	}
 
-	template<class T, class AT>
-	dr_vector_engine<T, AT>::dr_vector_engine(const dr_vector_engine<T, AT>& other):
-		alloc_(other.alloc_),
-		data_(std::allocator_traits<AT>::allocate(this->alloc_, other.cap_)),
-		elems_(other.elems_),
-		cap_(other.cap_)
+	template <class T, class AT>
+	dr_vector_engine<T, AT>::dr_vector_engine(const dr_vector_engine<T, AT>& other)
+		: alloc_(other.alloc_), data_(std::allocator_traits<AT>::allocate(this->alloc_, other.cap_)),
+		  elems_(other.elems_), cap_(other.cap_)
 	{
 		for (size_type i = 0; i < this->elems_; ++i)
 		{
@@ -124,135 +120,130 @@ namespace std::math
 		}
 	}
 
-	template<class T, class AT>
-	template<class U>
-	dr_vector_engine<T, AT>::dr_vector_engine(initializer_list<U> list):
-		dr_vector_engine<T, AT>(list.size())
+	template <class T, class AT>
+	template <class U>
+	dr_vector_engine<T, AT>::dr_vector_engine(initializer_list<U> list) : dr_vector_engine<T, AT>(list.size())
 	{
 		std::copy_n(list.begin(), list.size(), this->data_);
 	}
 
-	template<class T, class AT>
-	dr_vector_engine<T, AT>::dr_vector_engine(dr_vector_engine::size_type elems):
-		dr_vector_engine(elems, elems)
-	{}
-
-	template<class T, class AT>
-	dr_vector_engine<T, AT>::dr_vector_engine(dr_vector_engine::size_type elems, dr_vector_engine::size_type elem_cap):
-		alloc_(),
-		data_(std::allocator_traits<AT>::allocate(this->alloc_, elem_cap)),
-		elems_(elems),
-		cap_(elem_cap)
+	template <class T, class AT>
+	dr_vector_engine<T, AT>::dr_vector_engine(dr_vector_engine::size_type elems) : dr_vector_engine(elems, elems)
 	{
-		std::allocator_traits<AT>::construct(this->alloc_, this->data_, this->elems_);
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
+	dr_vector_engine<T, AT>::dr_vector_engine(dr_vector_engine::size_type elems, dr_vector_engine::size_type elem_cap)
+		: alloc_(), data_(std::allocator_traits<AT>::allocate(this->alloc_, elem_cap)), elems_(elems), cap_(elem_cap)
+	{
+		for (std::size_t i = 0; i < this->elems_; ++i)
+		{
+			std::allocator_traits<AT>::construct(this->alloc_, &this->data_[i], T{});
+		}
+	}
+
+	template <class T, class AT>
 	dr_vector_engine<T, AT>& dr_vector_engine<T, AT>::operator=(dr_vector_engine<T, AT>&& rhs) noexcept
 	{
 		return *this;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	dr_vector_engine<T, AT>& dr_vector_engine<T, AT>::operator=(const dr_vector_engine<T, AT>& rhs)
 	{
 		return *this;
 	}
 
-	template<class T, class AT>
-	template<class ET2>
+	template <class T, class AT>
+	template <class ET2>
 	dr_vector_engine<T, AT>& dr_vector_engine<T, AT>::operator=(const ET2& rhs)
 	{
 		return *this;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::iterator dr_vector_engine<T, AT>::begin() noexcept
 	{
 		return nullptr;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::iterator dr_vector_engine<T, AT>::end() noexcept
 	{
 		return nullptr;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::const_iterator dr_vector_engine<T, AT>::begin() const noexcept
 	{
 		return nullptr;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::const_iterator dr_vector_engine<T, AT>::end() const noexcept
 	{
 		return nullptr;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::const_iterator dr_vector_engine<T, AT>::cbegin() const noexcept
 	{
 		return nullptr;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::const_iterator dr_vector_engine<T, AT>::cend() const noexcept
 	{
 		return nullptr;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::size_type dr_vector_engine<T, AT>::capacity() const noexcept
 	{
 		return this->cap_;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::size_type dr_vector_engine<T, AT>::elements() const noexcept
 	{
 		return this->elems_;
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	void dr_vector_engine<T, AT>::reserve(dr_vector_engine::size_type cap)
 	{
-
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	void dr_vector_engine<T, AT>::resize(dr_vector_engine::size_type elems)
 	{
-
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	void dr_vector_engine<T, AT>::resize(dr_vector_engine::size_type elems, dr_vector_engine::size_type cap)
 	{
-
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	typename dr_vector_engine<T, AT>::reference dr_vector_engine<T, AT>::operator()(dr_vector_engine::size_type i)
 	{
 		return this->data_[i];
 	}
 
-	template<class T, class AT>
-	typename dr_vector_engine<T, AT>::const_reference dr_vector_engine<T, AT>::operator()(dr_vector_engine::size_type i) const
+	template <class T, class AT>
+	typename dr_vector_engine<T, AT>::const_reference
+	dr_vector_engine<T, AT>::operator()(dr_vector_engine::size_type i) const
 	{
 		return this->data_[i];
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	void dr_vector_engine<T, AT>::swap(dr_vector_engine& rhs) noexcept
 	{
-
 	}
 
-	template<class T, class AT>
+	template <class T, class AT>
 	void dr_vector_engine<T, AT>::swap_elements(dr_vector_engine::size_type i, dr_vector_engine::size_type j) noexcept
 	{
-
 	}
 }
--- test/src/dr_vector_engine.cpp
+++ test/src/dr_vector_engine.cpp
@@ -24,21 +24,13 @@ TEST_CASE("dr_vector_engine Default Constructor", "[dr_vector_engine][default][c
 		CHECK(engine.elements() == 0);
 		CHECK(engine.capacity() == 3);
 	}
-
-	SECTION("dr_vector_engine<std::vector<float>, std::allocator<std::vector<float>>>")
-	{
-		std::math::dr_vector_engine<std::vector<float>, std::allocator<std::vector<float>>> engine;
-
-		CHECK(engine.elements() == 0);
-		CHECK(engine.capacity() == 3);
-	}
 }
 
 TEST_CASE("dr_vector_engine Move Constructor", "[dr_vector_engine][move][constructor]")
 {
 	SECTION("dr_vector_engine<unsigned short, std::allocator<unsigned short>>")
 	{
-		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine1 (3, 6);
+		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine1(3, 6);
 
 		REQUIRE(engine1.elements() == 3);
 		CHECK(engine1.capacity() == 6);
@@ -51,7 +43,7 @@ TEST_CASE("dr_vector_engine Move Constructor", "[dr_vector_engine][move][constru
 		CHECK(engine1(1) == 2);
 		CHECK(engine1(2) == 1);
 
-		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine2 (std::move(engine1));
+		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine2(std::move(engine1));
 
 		REQUIRE(engine2.elements() == 3);
 		CHECK(engine2.capacity() == 6);
@@ -66,7 +58,7 @@ TEST_CASE("dr_vector_engine Move Constructor", "[dr_vector_engine][move][constru
 
 	SECTION("dr_vector_engine<float, std::allocator<float>>")
 	{
-		std::math::dr_vector_engine<float, std::allocator<float>> engine1 (3, 6);
+		std::math::dr_vector_engine<float, std::allocator<float>> engine1(3, 6);
 
 		REQUIRE(engine1.elements() == 3);
 		CHECK(engine1.capacity() == 6);
@@ -79,7 +71,7 @@ TEST_CASE("dr_vector_engine Move Constructor", "[dr_vector_engine][move][constru
 		CHECK(engine1(1) == Approx(2.2));
 		CHECK(engine1(2) == Approx(1.3));
 
-		std::math::dr_vector_engine<float, std::allocator<float>> engine2 (std::move(engine1));
+		std::math::dr_vector_engine<float, std::allocator<float>> engine2(std::move(engine1));
 
 		REQUIRE(engine2.elements() == 3);
 		CHECK(engine2.capacity() == 6);
@@ -97,66 +89,36 @@ TEST_CASE("dr_vector_engine Copy Constructor", "[dr_vector_engine][copy][constru
 {
 	SECTION("dr_vector_engine<unsigned short, std::allocator<unsigned short>>")
 	{
-		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine1 (3, 6);
+		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine1 {3, 6, 9, 12, 15};
 
-		REQUIRE(engine1.elements() == 3);
-		CHECK(engine1.capacity() == 6);
+		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine2(engine1);
 
-		engine1(0) = 3;
-		engine1(1) = 2;
-		engine1(2) = 1;
+		REQUIRE(engine2.elements() == engine1.elements());
+		CHECK(engine1.capacity() == engine2.capacity());
 
-		CHECK(engine1(0) == 3);
-		CHECK(engine1(1) == 2);
-		CHECK(engine1(2) == 1);
-
-		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine2 (engine1);
-
-		REQUIRE(engine2.elements() == 3);
-		CHECK(engine2.capacity() == 6);
-
-		CHECK(engine2(0) == 3);
-		CHECK(engine2(1) == 2);
-		CHECK(engine2(2) == 1);
-
-		REQUIRE(engine1.elements() == 3);
-		CHECK(engine1.capacity() == 6);
-
-		CHECK(engine1(0) == 3);
-		CHECK(engine1(1) == 2);
-		CHECK(engine1(2) == 1);
+		for (std::size_t i = 0; i < engine1.elements(); ++i)
+		{
+			CAPTURE(i);
+			CHECK(engine1(i) == Approx(3u * static_cast<unsigned short>(i + 1)));
+			CHECK(engine2(i) == Approx(3u * static_cast<unsigned short>(i + 1)));
+		}
 	}
 
 	SECTION("dr_vector_engine<float, std::allocator<float>>")
 	{
-		std::math::dr_vector_engine<float, std::allocator<float>> engine1 (3, 6);
+		std::math::dr_vector_engine<float, std::allocator<float>> engine1 {3.0f, 6.0f, 9.0f, 12.0f, 15.0f};
 
-		REQUIRE(engine1.elements() == 3);
-		CHECK(engine1.capacity() == 6);
+		std::math::dr_vector_engine<float, std::allocator<float>> engine2(engine1);
 
-		engine1(0) = 3.1;
-		engine1(1) = 2.2;
-		engine1(2) = 1.3;
+		REQUIRE(engine2.elements() == engine1.elements());
+		CHECK(engine1.capacity() == engine2.capacity());
 
-		CHECK(engine1(0) == Approx(3.1));
-		CHECK(engine1(1) == Approx(2.2));
-		CHECK(engine1(2) == Approx(1.3));
-
-		std::math::dr_vector_engine<float, std::allocator<float>> engine2 (engine1);
-
-		REQUIRE(engine2.elements() == 3);
-		CHECK(engine2.capacity() == 6);
-
-		CHECK(engine2(0) == Approx(3.1));
-		CHECK(engine2(1) == Approx(2.2));
-		CHECK(engine2(2) == Approx(1.3));
-
-		REQUIRE(engine1.elements() == 3);
-		CHECK(engine1.capacity() == 6);
-
-		CHECK(engine1(0) == Approx(3.1));
-		CHECK(engine1(1) == Approx(2.2));
-		CHECK(engine1(2) == Approx(1.3));
+		for (std::size_t i = 0; i < engine1.elements(); ++i)
+		{
+			CAPTURE(i);
+			CHECK(engine1(i) == Approx(3.0f * (static_cast<float>(i) + 1.0f)));
+			CHECK(engine2(i) == Approx(3.0f * (static_cast<float>(i) + 1.0f)));
+		}
 	}
 }
 
@@ -164,26 +126,28 @@ TEST_CASE("dr_vector_engine Initializer List Constructor", "[dr_vector_engine][i
 {
 	SECTION("dr_vector_engine<unsigned short, std::allocator<unsigned short>>")
 	{
-		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine {3, 6, 9, 12, 15};
+		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine{3, 6, 9, 12, 15};
 
 		REQUIRE(engine.elements() == 5);
 		CHECK(engine.capacity() >= 5);
 
 		for (std::size_t i = 0; i < engine.elements(); ++i)
 		{
+			CAPTURE(i);
 			CHECK(engine(i) == ((i + 1) * 3));
 		}
 	}
 
 	SECTION("dr_vector_engine<float, std::allocator<float>>")
 	{
-		std::math::dr_vector_engine<float, std::allocator<float>> engine {4.0, 8.0, 12.0, 16.0, 20.0};
+		std::math::dr_vector_engine<float, std::allocator<float>> engine{4.0, 8.0, 12.0, 16.0, 20.0};
 
 		REQUIRE(engine.elements() == 5);
 		CHECK(engine.capacity() >= 5);
 
 		for (std::size_t i = 0; i < engine.elements(); ++i)
 		{
+			CAPTURE(i);
 			CHECK(engine(i) == Approx(((i + 1) * 4.0)));
 		}
 	}
@@ -193,32 +157,60 @@ TEST_CASE("dr_vector_engine Single Argument Constructor", "[dr_vector_engine][si
 {
 	SECTION("dr_vector_engine<unsigned short, std::allocator<unsigned short>>")
 	{
-		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine (3);
+		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine(3);
 
 		REQUIRE(engine.elements() == 3);
 		CHECK(engine.capacity() >= 3);
 
 		for (std::size_t i = 0; i < engine.elements(); ++i)
 		{
+			CAPTURE(i);
 			CHECK(engine(i) == 0);
 		}
 	}
 
 	SECTION("dr_vector_engine<float, std::allocator<float>>")
 	{
-		std::math::dr_vector_engine<float, std::allocator<float>> engine (3);
+		std::math::dr_vector_engine<float, std::allocator<float>> engine(3);
 
 		REQUIRE(engine.elements() == 3);
 		CHECK(engine.capacity() >= 3);
 
 		for (std::size_t i = 0; i < engine.elements(); ++i)
 		{
-			CHECK(engine(i) == Approx(0.0));
+			CAPTURE(i);
+			CHECK(engine(i) == Approx(0.0f));
 		}
 	}
 }
 
 TEST_CASE("dr_vector_engine Two Argument Constructor", "[dr_vector_engine][two][argument][constructor]")
 {
+	SECTION("dr_vector_engine<unsigned short, std::allocator<unsigned short>>")
+	{
+		std::math::dr_vector_engine<unsigned short, std::allocator<unsigned short>> engine(3, 6);
 
+		REQUIRE(engine.elements() == 3);
+		CHECK(engine.capacity() >= 6);
+
+		for (std::size_t i = 0; i < engine.elements(); ++i)
+		{
+			CAPTURE(i);
+			CHECK(engine(i) == 0);
+		}
+	}
+
+	SECTION("dr_vector_engine<float, std::allocator<float>>")
+	{
+		std::math::dr_vector_engine<float, std::allocator<float>> engine(3, 6);
+
+		REQUIRE(engine.elements() == 3);
+		CHECK(engine.capacity() >= 6);
+
+		for (std::size_t i = 0; i < engine.elements(); ++i)
+		{
+			CAPTURE(i);
+			CHECK(engine(i) == Approx(0.0f));
+		}
+	}
 }
